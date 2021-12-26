20e3e78b3ac93d57c6381e87b625cb7d869df737
Kalender Hisir <hisirkalender@gmail.com>
2021-12-26 21:59:19 +0100

    settings added...

--- src/app/cc-sidebar/cc-sidebar.component.ts
+++ src/app/cc-sidebar/cc-sidebar.component.ts
@@ -8,18 +8,31 @@ import { Component, Input, OnInit } from '@angular/core';
 export class CcSidebarComponent implements OnInit {
   @Input() showSidebar: boolean = false;
   @Input() showOverlay: boolean = true; // used only when fixed is false!
-  @Input() fixed: boolean = true;
+  @Input() fixed: boolean = false;
   @Input() position: string = 'left'; // left or right
 
   showSettings: boolean = false;
+  componentId: string;
 
   constructor() {
+    this.componentId = this.createComponentId();
     if (this.position !== 'left' && this.position !== 'right') {
       this.position = 'left';
     }
   }
 
   ngOnInit() {
+  }
+
+  createComponentId() {
+    // tslint:disable-next-line:only-arrow-functions
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      // tslint:disable-next-line:no-bitwise
+      const r = Math.random() * 16 | 0;
+      // tslint:disable-next-line:no-bitwise
+      const v = c === 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+    });
   }
 
 }
