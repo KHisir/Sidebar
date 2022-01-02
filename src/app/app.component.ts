e86bcf843d6f92094150a6dbea37316047bba127
Kalender Hisir <hisirkalender@gmail.com>
2022-01-02 23:22:12 +0100

    changed all...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cc-sidebar';
+  showSidebar: boolean = false;
+  showSidebar2: boolean = false;
+
+  show(): void {
+    this.showSidebar = true
+  }
+
+  show2(): void {
+    this.showSidebar2 = true
+  }
 }
--- src/app/cc-sidebar/cc-sidebar.component.ts
+++ src/app/cc-sidebar/cc-sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, HostListener, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-cc-sidebar',
@@ -6,20 +6,37 @@ import { Component, HostListener, Input, OnInit } from '@angular/core';
   styleUrls: ['./cc-sidebar.component.scss']
 })
 export class CcSidebarComponent implements OnInit {
-  @Input() showSidebar: boolean = false;
-  @Input() showOverlay: boolean = false; // used only when fixed is false!
+  private _visible: boolean = false;
+
+  @Input() overlay: boolean = false; // used only when fixed is false!
   @Input() fixed: boolean = true;
   @Input() position: string = 'left'; // left or right
-  @Input() sidebarMinWidth: number = 250;
+  @Input() configureable: boolean = true;
+  @Input() showCloseButton: boolean = true;
+  @Input() width: number = 250;
+  @Input() minWidth: number = 250;
+  @Input() opacity: number = 1;
+  @Input() zIndex: number = 999;
+  @Input() backgroundColor: string = "#000";
+  @Input() pageElem?: HTMLElement;
+  @Input()
+  public set visible(value: boolean) {
+    this._visible = value;
+    this.handlePage();
+  }
+  @Output() visibleChange: EventEmitter<boolean> = new EventEmitter<boolean>();
 
-  showSettings: boolean = false;
+  showConfig: boolean = false;
   componentId: string;
-  sidebarWidth: number = 250;
-  sidebarMaxWidth: number = (90 / 100) * window.innerWidth; // get 90% of screen width.
+  maxWidth: number = (90 / 100) * window.innerWidth; // get 90% of screen width.
+
+  public get visible(): boolean {
+    return this._visible;
+  }
 
   @HostListener('window:resize', ['$event'])
   onResize(event: any) {
-    this.sidebarMaxWidth = (90 / 100) * window.innerWidth; // get 90% of screen width.
+    this.maxWidth = (90 / 100) * window.innerWidth; // get 90% of screen width.
   }
 
   constructor() {
@@ -29,13 +46,67 @@ export class CcSidebarComponent implements OnInit {
     }
   }
 
-  ngOnInit() {
-    document.documentElement.style.setProperty('--sidebar-min-width', (this.sidebarMinWidth + 'px'));
-    document.documentElement.style.setProperty('--sidebar-width', '250px');
+  ngOnInit() {}
+
+  ngOnDestroy() {
+    if (this.pageElem !== undefined) {
+      this.pageElem.style.width = '';
+    }
   }
 
-  sidebarWidthChanged(): void {
-    document.documentElement.style.setProperty('--sidebar-width', (this.sidebarWidth + 'px'));
+  close(): void {
+    this.visible = !(this.visible); 
+    this.visibleChange.emit(this.visible);
+  }
+
+  widthChanged(): void {
+    // document.documentElement.style.setProperty('--sidebar-width', (this.width + 'px'));
+    this.handlePage();
+  }
+
+  getMarginVal(): number {
+    if (this.visible === true) {
+      return 0;
+    } else {
+      return -this.width;
+    }
+  }
+
+  handlePage(positionChanged: boolean = false): void {
+    if (this.pageElem !== undefined) {
+      this.pageElem.style.width = 'auto';
+      if (this.fixed === true) {
+        if (this.visible === true) {
+          // this.pageElem.style.width = 'calc(100% - ' + this.width + 'px)';
+          // this.pageElem.style.width = (this.pageElem.clientWidth - this.width) + 'px';
+          if (this.position === 'left') {
+            if (positionChanged === true) {
+              this.pageElem.style.marginRight = '';
+            }
+            this.pageElem.style.marginLeft = this.width + 'px';
+          } else {
+            if (positionChanged === true) {
+              this.pageElem.style.marginLeft = '';
+            }
+            this.pageElem.style.marginRight = this.width + 'px';
+          }  
+        } else {
+          // this.pageElem.style.width = '';
+          if (this.position === 'left') {
+            this.pageElem.style.marginLeft = '';
+          } else {
+            this.pageElem.style.marginRight = '';
+          }
+        }
+      } else {
+        // this.pageElem.style.width = '';
+        if (this.position === 'left') {
+          this.pageElem.style.marginLeft = '';
+        } else {
+          this.pageElem.style.marginRight = '';
+        }
+      }
+    }
   }
 
   createComponentId() {
